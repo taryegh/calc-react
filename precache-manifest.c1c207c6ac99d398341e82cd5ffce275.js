428dcb53716cd14b402447b1bc95c70cc74612fe
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-15 22:39:04 +0400

    Updates

---
+++ precache-manifest.c1c207c6ac99d398341e82cd5ffce275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bab387c5643c98493899e94f3d8f0d7",
+    "url": "/calc-react/index.html"
+  },
+  {
+    "revision": "04df4d3661909a4d1b90",
+    "url": "/calc-react/static/css/main.f781e8f5.chunk.css"
+  },
+  {
+    "revision": "27ccd2623cce71a3e403",
+    "url": "/calc-react/static/js/2.9966f13b.chunk.js"
+  },
+  {
+    "revision": "04df4d3661909a4d1b90",
+    "url": "/calc-react/static/js/main.f92d66d8.chunk.js"
+  },
+  {
+    "revision": "54fe2bc16b4c389abbdf",
+    "url": "/calc-react/static/js/runtime~main.ca02e7f1.js"
+  }
+]);
